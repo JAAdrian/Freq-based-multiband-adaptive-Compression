bd9a5c45bb9588a09d8f994df2c0363a02246c4e
Jens-Alrik Adrian <jensalrik.adrian@gmail.com>
2025-12-24 19:39:31 +0100

    Add docstrings

--- tests/test_compression.py
+++ tests/test_compression.py
@@ -10,10 +10,12 @@ TEST_SIGNAL_FILEPATH = r"tests/audio/61-70968-0049.flac"
 
 @pytest.fixture
 def audio_signal():
+    """Read test audio signal."""
     return soundfile.read(TEST_SIGNAL_FILEPATH)
 
 
 def test_compression(audio_signal):
+    """Test whether the multiband compression returns a signal."""
     signal, sample_rate = audio_signal
     gains = 9 * (0)
 
