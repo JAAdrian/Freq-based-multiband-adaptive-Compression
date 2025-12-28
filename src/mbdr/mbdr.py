8f5daa62057bfa7e6c3e8302e54fabdaf915d5d7
Jens-Alrik Adrian <jensalrik.adrian@gmail.com>
2025-12-29 00:56:22 +0100

    Implement gain smoothing

--- src/mbdr/mbdr.py
+++ src/mbdr/mbdr.py
@@ -16,13 +16,25 @@ import numpy
 from mbdr import filterbank as fbank
 from mbdr import utils
 
-SAMPLE_RATE = 16_000
+DEFAULT_SAMPLE_RATE = 16_000
+DEFAULT_COMPRESSION_THRESHOLD = -30
+DEFAULT_COMPRESSION_RATIO = 5
+DEFAULT_KNEE_WIDTH = 4
+DEFAULT_MAKEUP_GAIN = 0
+DEFAULT_ATTACK_TIME_SEC = 20e-3
+DEFAULT_RELEASE_TIME_SEC = 1e-1
+
 CENTER_FREQUENCIES_HZ = (250, 500, 1_000, 1_500, 2_000, 3_000, 4_000, 6_000, 8_000)
 
 BLOCK_SIZE_SEC = 32e-3
 OVERLAP_RATIO = 0.5
 
 FLUX_SMOOTHING_CONSTANT_SEC = 100e-3
+FLUX_EPSILON = 1e-16
+FLUX_GAMMA = 4
+
+MINIMUM_RELEASE_TIME_SEC = 0.2
+MAXIMUM_RELEASE_TIME_SEC = 2
 
 DEBUG = False
 
@@ -30,11 +42,12 @@ DEBUG = False
 def compress_signal(
     signal: numpy.ndarray,
     prescriptive_gains: tuple[int],
-    compression_threshold: int,
-    compression_ratio: int,
-    knee_width: int,
-    makeup_gain: int = 0,
-    sample_rate: int = SAMPLE_RATE,
+    compression_threshold: int = DEFAULT_COMPRESSION_THRESHOLD,
+    compression_ratio: int = DEFAULT_COMPRESSION_RATIO,
+    knee_width: int = DEFAULT_KNEE_WIDTH,
+    makeup_gain: int = DEFAULT_MAKEUP_GAIN,
+    attack_time: float = DEFAULT_ATTACK_TIME_SEC,
+    sample_rate: int = DEFAULT_SAMPLE_RATE,
 ) -> numpy.ndarray:
     """Compress the input signal based on freq-based multiband adaptive compression."""
     fb = fbank.Filterbank(
@@ -72,6 +85,7 @@ def compress_signal(
         signal_magnitudes=numpy.abs(stft),
         compression_function=compression_function,
         hop_size=fb.hop_size,
+        attack_time=attack_time,
         sample_rate=sample_rate,
     )
 
@@ -143,9 +157,12 @@ def _smooth_gains(
     signal_magnitudes: numpy.ndarray,
     compression_function: numpy.ndarray,
     hop_size: int,
+    attack_time: float,
     sample_rate: int,
 ) -> numpy.ndarray:
     power_spectrum = 20 * numpy.log10(signal_magnitudes)
+    compression_residual = compression_function - power_spectrum
+
     flux = utils.spectral_flux(magnitudes=signal_magnitudes)
 
     frame_rate = sample_rate // hop_size
@@ -155,5 +172,31 @@ def _smooth_gains(
         sample_rate=frame_rate,
     )
     smoothed_flux = numpy.fromiter(smoother, dtype=float)
+    adaptive_release_time = numpy.maximum(
+        MINIMUM_RELEASE_TIME_SEC,
+        MINIMUM_RELEASE_TIME_SEC / (smoothed_flux**FLUX_GAMMA + FLUX_EPSILON),
+    )
+    adaptive_release_time = numpy.minimum(
+        MAXIMUM_RELEASE_TIME_SEC, adaptive_release_time
+    )
+    alphas_release = 1 - numpy.exp(-1 / (adaptive_release_time * frame_rate))
 
-    return numpy.zeros_like(compression_function)
+    smoothed_gains = list()
+    alpha_attack = 1 - numpy.exp(-1 / (attack_time * frame_rate))
+    for index, (residual, alpha_release) in enumerate(
+        zip(compression_residual[:, 1:].T, alphas_release)
+    ):
+        compression_value = compression_function[:, index]
+
+        new_gain = numpy.zeros_like(residual)
+        new_gain[residual > compression_value] = (
+            alpha_attack * residual + (1 - alpha_attack) * compression_value
+        )
+        new_gain[residual >= compression_value] = (
+            alpha_release * residual + (1 - alpha_release) * compression_value
+        )
+
+        smoothed_gains.append(new_gain)
+
+    smoothed_gains.append(smoothed_gains[-1])
+    return numpy.stack(smoothed_gains, axis=1)
